aa0bb5394ca1f601313764446c84c274bb145222
Abdullah <mabdullahafzal09@gmail.com>
2022-07-04 12:47:54 +0500

    Encrytion and Decryption using Crypto-JS (sha 256)

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
+// const express = require("express");
+// const bcrypt = require("bcrypt");
+
+// const app = express();
+
+// console.log("It's Happening !!!");
+// app.use(express.json());
+
+// const isValid = (req, res, next) => {
+//   console.log(req.body.password);
+//   if (req.body.email.length != 0 && req.body.password != 0) next();
+//   else res.send("One or more Inputs are Empty !");
+// };
+// app.use(isValid, async (req, res, next) => {
+//   const salt = await bcrypt.genSalt(10);
+//   const secPass = await bcrypt.hash(req.body.password, salt);
+//   console.log(secPass);
+//   console.log("It's in the hashing Middleware");
+//   res.send("Got te Response");
+// });
+// app.listen(3000, () => console.log("App is listening on 3000 port"));
+
 const express = require("express");
-const bcrypt = require("bcrypt");
+const CryptoJS = require("crypto-js");
 
 const app = express();
 
-console.log("It's Happening !!!");
 app.use(express.json());
 
 const isValid = (req, res, next) => {
   console.log(req.body.password);
   if (req.body.email.length != 0 && req.body.password != 0) next();
-  else res.send("One or more Inputs are Empty !");
+  else res.send("email or password is Empty !");
 };
 app.use(isValid, async (req, res, next) => {
-  const salt = await bcrypt.genSalt(10);
-  const secPass = await bcrypt.hash(req.body.password, salt);
-  console.log(secPass);
-  console.log("It's in the hashing Middleware");
+  const secretKey = ")V@Ln3_Az=+G9xr+";
+  //Encryption
+  const cypherText = CryptoJS.AES.encrypt(
+    req.body.password,
+    secretKey
+  ).toString();
+  console.log(cypherText);
   res.send("Got te Response");
 });
 app.listen(3000, () => console.log("App is listening on 3000 port"));
